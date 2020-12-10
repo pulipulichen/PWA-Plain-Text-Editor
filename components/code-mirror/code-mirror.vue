65358a45da4887f816d5c92a288735335e149989
Pulipuli Chen <blog@pulipuli.info>
2020-12-10 16:34:11 +0800

    1210 #8 總算有個雛形了，好慘

--- components/code-mirror/code-mirror.vue
+++ components/code-mirror/code-mirror.vue
@@ -2,6 +2,7 @@
 <!-- =================================== -->
 
 <template src="./components/code-mirror/code-mirror.html"></template>
-<style src="./components/code-mirror/code-mirror.less" lang="less"></style>
+<style src="./components/code-mirror/code-mirror.global.less" lang="less"></style>
+<style src="./components/code-mirror/code-mirror.scoped.less" lang="less" scoped></style>
 <script src="./components/code-mirror/code-mirror.js"></script>
 
